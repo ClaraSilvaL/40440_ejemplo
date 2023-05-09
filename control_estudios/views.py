19d4a2c31d69c54b547b2a5487a3ce6974325775
Clara Silva <clara.silvaleon1@gmail.com>
2023-05-09 17:40:02 -0500

    Agregado formulario crear curso usando Django forms

--- control_estudios/views.py
+++ control_estudios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from control_estudios.forms import CursoFormulario
 from control_estudios.models import Estudiante, Curso
 
 # Create your views here.
@@ -22,5 +26,51 @@ def listar_cursos(request):
         request = request,
         template_name='control_estudios/lista_cursos.html',
         context = contexto,
+    )
+    return http_response
+
+def crear_curso_version_1(request):
+    #No la estamos usando
+    if request.method =="POST":
+        data = request.POST #es un diccionario
+        nombre = data["nombre"]
+        comision = data["comision"]
+        curso = Curso.objects.create(nombre=nombre, comision = comision) #Lo crean solo en RAM
+        curso.save() #Se guarda en la base de datos
+        
+        # Redirecciono al usuario a la lista de cursos
+        url_exitosa = reverse('lista_cursos') #estudios/cursos
+        return redirect(url_exitosa)
+    else: #GET
+    #contexto = {}
+        http_response = render(
+            request = request,
+            template_name='control_estudios/formulario_curso_a_mano.html',
+            #context = contexto,
+        )
+        return http_response
+
+def crear_curso(request):
+    if request.method =="POST":
+        formulario = CursoFormulario(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data #es un diccionario
+            nombre = data["nombre"]
+            comision = data["comision"]
+            curso = Curso(nombre=nombre, comision = comision) #Lo crean solo en RAM
+            curso.save() #Se guarda en la base de datos
+        
+            # Redirecciono al usuario a la lista de cursos
+            url_exitosa = reverse('lista_cursos') #estudios/cursos
+            return redirect(url_exitosa)
+        
+    else: #GET
+        formulario = CursoFormulario(initial=request.POST)
+    #contexto = {}
+    http_response = render(
+        request = request,
+        template_name='control_estudios/formulario_curso_a_mano.html',
+        context = {'formulario': formulario},
     )
     return http_response
